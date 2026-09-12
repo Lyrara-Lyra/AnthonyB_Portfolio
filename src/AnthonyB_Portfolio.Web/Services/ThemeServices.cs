f75b8f1bc5b26011d9edf78013a03e51d4351cd2
Lyrara Lyra <jamesjeux007@hotmail.com>
2026-09-12 18:44:47 +0200

    Projects styling + misc cleanup

--- src/AnthonyB_Portfolio.Web/Services/ThemeServices.cs
+++ src/AnthonyB_Portfolio.Web/Services/ThemeServices.cs
@@ -5,8 +5,9 @@ namespace AnthonyB_Portfolio.Web.Services
     // Handles toggling between themes
     public class ThemeService
     {
-        private const string StorageLabel = "selected-theme"; 
+        private const string _storageLabel = "selected-theme"; 
         private readonly IJSRuntime _jsRunTime;
+        private bool _isTransitioning = false;
         public string CurrentTheme { get; private set; } = "";
         public event Action? OnChange;
 
@@ -19,7 +20,7 @@ namespace AnthonyB_Portfolio.Web.Services
         public async Task InitializeAsync()
         {
             // Get the saved theme from LocalStorage
-            var savedTheme = await _jsRunTime.InvokeAsync<string>("localStorage.getItem", StorageLabel);
+            var savedTheme = await _jsRunTime.InvokeAsync<string>("localStorage.getItem", _storageLabel);
 
             if (!string.IsNullOrEmpty(savedTheme))
             {
@@ -35,10 +36,35 @@ namespace AnthonyB_Portfolio.Web.Services
         // Toggles the theme and saves it to local storage
         public async Task ToggleThemeAsync()
         {
-            CurrentTheme = CurrentTheme == "dark" ? "light" : "dark";
-            
-            await _jsRunTime.InvokeVoidAsync("localStorage.setItem", StorageLabel, CurrentTheme);
-            NotifyStateChanged();
+            // Abort if a transition is currently in progress
+            if (_isTransitioning) return;
+
+            // Transition between themes
+            try
+            {
+                // Toggle the currently used theme
+                _isTransitioning = true;
+                CurrentTheme = CurrentTheme == "dark" ? "light" : "dark";
+
+                // Save the new theme in local storage
+                await _jsRunTime.InvokeVoidAsync("localStorage.setItem", _storageLabel, CurrentTheme);
+                NotifyStateChanged();
+
+                // Add a temporary class to transition between themes
+                await _jsRunTime.InvokeVoidAsync("document.documentElement.classList.add", "theme-change");
+
+                // Theme transition time
+                await Task.Delay(500);
+            }
+            // Always remove the lock at the end
+            finally
+            {
+                // Remove the temporary class
+                await _jsRunTime.InvokeVoidAsync("document.documentElement.classList.remove", "theme-change");
+
+                // Transition is over
+                _isTransitioning = false;
+            }
         }
 
         // Updates the subscribers on state change
